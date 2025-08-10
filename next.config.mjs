6ca306f79e6cb37c9fc5745e6d6ca31fa78c422f
Mustafa Uçar <m.mustafa.ucr@gmail.com>
2025-08-10 14:20:18 +0300

    Remove unused rewrites and update development script in configuration files

--- next.config.mjs
+++ next.config.mjs
@@ -7,27 +7,6 @@ const nextConfig = {
     })
     return config
   },
-  output: 'standalone',
-  async rewrites() {
-    const SIGNIN_URL = process.env.NEXT_PUBLIC_SIGNIN_URL
-    return [
-      {
-        source: '/signin/:path*',
-        destination: `${SIGNIN_URL}/auth/signin/:path*`
-      },
-      {
-        source: '/signout',
-        destination: `${SIGNIN_URL}/auth/signout`
-      },
-      {
-        source: '/otp/:path*',
-        destination: `${SIGNIN_URL}/otp/:path*`
-      },
-      {
-        source: '/set-cookies/:path*',
-        destination: `${SIGNIN_URL}/set-cookies/:path*`
-      }
-    ]
-  }
+  output: 'standalone'
 }
 export default nextConfig
