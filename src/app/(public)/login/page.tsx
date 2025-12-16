7fdcb4b8289718b3b8b91133d03dbfde84e3c87a
mustafa_ucar <muhammedmustafaucar@fiyuu.com.tr>
2025-12-16 11:33:46 +0300

    Add useEffect to LoginPage for history state management
    Implement useEffect to replace the current history state on the LoginPage, ensuring a cleaner navigation experience when users access the login form.

--- src/app/(public)/login/page.tsx
+++ src/app/(public)/login/page.tsx
@@ -1,8 +1,13 @@
 'use client'
 import { AuthProvider } from '@/modules/auth/context/auth-context'
 import { LoginView } from '@/modules/auth/views/login'
+import { useEffect } from 'react'
 
 export default function LoginPage() {
+  useEffect(() => {
+    window.history.replaceState(null, '', window.location.href)
+  }, [])
+
   return (
     <AuthProvider>
       <LoginView />
