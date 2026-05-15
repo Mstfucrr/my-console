59efafe965f7ca75e3a1cdb5e292a0123cff28fb
mustafa_ucar <muhammedmustafaucar@fiyuu.com.tr>
2026-05-15 12:53:23 +0300

    Enhance button component styles for improved interaction and accessibility
    - Updated button variants to include a transition effect for smoother scaling on focus.
    - Adjusted the success button styling to ensure a consistent disabled state appearance, enhancing visual feedback for users.

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -5,14 +5,14 @@ import * as React from 'react'
 import { cn } from '@/lib/utils'
 
 const buttonVariants = cva(
-  'ring-offset-background inline-flex items-center justify-center rounded-md text-sm font-semibold whitespace-nowrap transition-colors focus-visible:ring-0 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-70',
+  'ring-offset-background inline-flex items-center justify-center rounded-md text-sm font-semibold whitespace-nowrap transition-all duration-200 focus:scale-105 focus-visible:ring-0 focus-visible:outline-none disabled:pointer-events-none disabled:opacity-70',
   {
     variants: {
       color: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/80',
         primary: 'bg-primary text-primary-foreground hover:bg-primary/80',
         destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/80',
-        success: 'bg-success text-success-foreground hover:bg-success/80 focus:bg-success/80',
+        success: 'bg-success text-success-foreground hover:bg-success/80 disabled:bg-success/80 focus:bg-success/80',
         info: 'bg-info text-info-foreground hover:bg-info/80',
         warning: 'bg-warning text-warning-foreground hover:bg-warning/80',
         secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
